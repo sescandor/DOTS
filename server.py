553737fd2123d97942f9f2122783409605a55c1c
Sandra Escandor-O'Keefe <sokeefe@fastly.com>
2017-02-15 07:32:00 -0330

    rough outline for server.py - needs to be fixed up.

--- server.py
+++ server.py
@@ -1,8 +1,29 @@
 #! /usr/bin/python
 
+import random
+import sys
+import comm_channel
 import DOTSServerMessage_pb2
 import DOTSClientMessage_pb2
-import sys
+
+
+class DOTSServer(object):
+
+    def __init__(self, channel):
+        self.client_message = DOTSClientMessage_pb2.DOTSClientMessage()
+        self.server_message = DOTSServerMessage_pb2.DOTSServerMessage()
+        self.server_message.seqno = 8
+        self.last_recv_seqno = 0
+        self.server_message.last_client_seqno = self.last_recv_seqno
+        self.channel = channel
+
+    def writebuf(self):
+        self.server_message.seqno = self.server_message.seqno + 1
+        self.server_message.last_client_seqno = self.last_recv_seqno
+
+    def send(self):
+        self.writebuf()
+        self.channel.write(self.server_message.SerializeToString())
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
