edf62837a8b8299e3077d0db03e9f795e96ad8a4
Sandra Escandor-O'Keefe <sokeefe@fastly.com>
2017-02-15 07:22:56 -0330

    Add read to client

--- client.py
+++ client.py
@@ -4,32 +4,48 @@ import random
 import sys
 import comm_channel
 import DOTSClientMessage_pb2
+import DOTSServerMessage_pb2
 
 
 class DOTSClient(object):
 
-    def __init__(self, channel_handle):
-        self.message = DOTSClientMessage_pb2.DOTSClientMessage()
-        self.message.seqno = random.randint(0, 18446744073709551615)
-        self.channel = comm_channel.CommChannel(channel_handle)
+    def __init__(self, channel):
+        self.client_message = DOTSClientMessage_pb2.DOTSClientMessage()
+        self.server_message = DOTSServerMessage_pb2.DOTSServerMessage()
+        self.client_message.seqno = random.randint(0, 18446744073709551615)
+        self.last_recv_seqno = 0
+        self.client_message.last_svr_seqno = self.last_recv_seqno
+        self.channel = channel
 
     def writebuf(self):
-        self.message.seqno = self.message.seqno + 1
+        self.client_message.seqno = self.client_message.seqno + 1
+        self.client_message.last_svr_seqno = self.last_recv_seqno
 
     def send(self):
         self.writebuf()
 
-        self.channel.write(self.message.SerializeToString())
+        self.channel.write(self.client_message.SerializeToString())
+
+    def readbuf(self):
+        self.server_message.ParseFromString(self.channel.read())
+
+        self.last_recv_seqno = self.server_message.seqno
+
+    def read(self):
+        self.readbuf()
 
     def close_channel(self):
         self.channel.close()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "Usage:", sys.argv[0], "COMM_FILE"
+    if len(sys.argv) != 3:
+        print "Usage:", sys.argv[0], "SEND_COMM_FILE RECV_COMM_FILE"
         sys.exit(-1)
 
-    client = DOTSClient(sys.argv[1])
+    channel = comm_channel.CommChannel(sys.argv[1], sys.argv[2])
+
+    client = DOTSClient(channel)
     client.send()
+    # client.read()
     client.close_channel()
--- comm_channel.py
+++ comm_channel.py
@@ -3,11 +3,16 @@
 
 class CommChannel(object):
 
-        def __init__(self, handle):
-            self.medium = open(handle, "wb")
+        def __init__(self, send_handle, recv_handle):
+            self.medium_send = open(send_handle, "wb")
+            self.medium_recv = open(recv_handle, "w+")
 
         def write(self, message):
-            self.medium.write(message)
+            self.medium_send.write(message)
+
+        def read(self):
+            return self.medium_recv.read()
 
         def close(self):
-            self.medium.close()
+            self.medium_send.close()
+            self.medium_recv.close()
